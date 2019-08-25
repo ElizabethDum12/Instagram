5e1f767507c9afef31d5245a6dd297e2b76c706e
ElizabethDum12 <elizabethdumervil@gmail.com>
2019-08-25 18:19:14 -0400

    data object and query defined

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -3,12 +3,13 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ParseObject.registerSubclass(Post.class);
         //Set applicationID, and sever based on the values in the Heroku settings.
         //clientKey is not needed unless explicity congigured
         //any network interceptors must be added with the Configuration builfer given this syntax
